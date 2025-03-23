4f9869a570b0803664160eab4f47458953ec5afb
Nifox-official <russkiy.ejik@mail.ru>
2025-03-23 13:26:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pygame import *
+from random import *
 class GameSprite(sprite.Sprite):
     def __init__(self, img,x,y,w,h,speed):
         super().__init__()
@@ -19,18 +20,57 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if key_pressed[K_s]and self.rect.y<400:
             self.rect.y += self.speed
+    def update_right(self):
+        key_pressed=key.get_pressed()
+        if key_pressed[K_UP]and self.rect.y>0:
+            self.rect.y -= self.speed
+        if key_pressed[K_DOWN]and self.rect.y<400:
+            self.rect.y += self.speed
+
+class Ball(GameSprite):
+    def __init__(self, img,x,y,w,h,speed):
+        super().__init__(img,x,y,w,h,speed)
+        self.direct = [0,0]
+    def update(self):
+        global score_l,score_r
+        self.rect.x += self.speed*self.direct[0]
+        self.rect.y += self.speed*self.direct[1]
+        if self.rect.y <= 0 or self.rect.y>=500-self.rect.height:
+            self.direct[1]*= -1
+        # if self.rect.x <= 0 or self.rect.x>=700-self.rect.height:
+        #     self.direct[0]*= -1
+        if self.rect.colliderect(player1) or self.rect.colliderect(player2):
+            self.direct[0]*= -1
+        if self.rect.x<=0:
+            score_r +=1
+            self.start()
+        if self.rect.x>=700-self.rect.width:
+            score_r +=1
+            self.start()
+        
+
+    def start(self):
+        self.rect.x = 310
+        self.rect.y = 210
+        ball.direct[0]= choice([-1,1])
+        ball.direct[1]= choice([-1,1])
 
 window=display.set_mode((700,500))
-display.set_caption('Ping Pong')
+display.set_caption('Cat Pong')
 background = transform.scale(image.load('backround.png'),(700 ,500))
-player=Player('left_cat.png', 250,400,100,100,5)
+player1=Player('left_cat.png', 0,200,100,100,5)
+player2=Player('right_cat.png', 600,200,100,100,5)
+ball=Ball('ball.png', 350-40,250-40,80,80,5)
+ball.direct[0]= choice([-1,1])
+ball.direct[1]= choice([-1,1])
 mixer.init()
 mixer.music.load('music.mp3')
-mixer.music.play(0)
+mixer.music.play(-1)
 clock = time.Clock()
 fps=60
-
-
+score_l=0
+score_r=0
+rule = 3
 game = True
 finish=True
 
@@ -40,10 +80,12 @@ while game:
             game = False
     
     window.blit(background, (0,0))
-    player.reset()
-    player.update_left()
-
-
+    player1.reset()
+    player1.update_left()
+    player2.reset()
+    player2.update_right()
+    ball.reset()
+    ball.update()
 
     display.update()
     clock.tick(fps)
